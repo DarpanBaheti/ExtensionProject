14f0cef71be465cf2812cbba4151e6f6395a8a03
darpan.b <darpan.b@tempdarpanb.internal.directi.com>
2018-10-24 15:59:34 +0530

    Twitter Oauth window close issue solved

--- newtab/apiFunctions/twitterOAuth.js
+++ newtab/apiFunctions/twitterOAuth.js
@@ -2,8 +2,8 @@
 function handleTwitterOAuth(widgetKey) {
     $("#container").off("click", "#" + "Twitter-signIn");
     $("#container").on("click", "#" + "Twitter-signIn", function(e) {
-        checkSuccessfulOauth(e);
-        oAuthTwitter();
+        // checkSuccessfulOauth(e);
+        oAuthTwitter(e);
     });
 
     $("#container").off("click", "#" + "Twitter-signOut");
@@ -15,41 +15,58 @@ function handleTwitterOAuth(widgetKey) {
     });
 }
 
-function oAuthTwitter() {
+function oAuthTwitter(e) {
     var uniqueUserId = localStorage.getItem("uniqueUserId");
     twitterSignInUrl = 'http://localhost:8081/signInWithTwitter' + "?userId=" + uniqueUserId;
     twitterOAuthWindow = window.open(twitterSignInUrl, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
     twitterOAuthWindow.focus();
-}
 
-function checkSuccessfulOauth(e) {
     var twitterSignInButtonEl = document.getElementById(e.target.id);
-
-    /* Cross Origin Listener */
-    // window.addEventListener('message', function(e) {
-    //     var message = e.data;
-    //     if(e.origin === "http://localhost:8000")
-    //         console.log(message + " " + e.origin);
-    // });
-
-    /* Local Storage Listner */
-    $(window).unbind('storage');
-    $(window).bind('storage', function (e) {
-        widgetKey = "Twitter";
-        const localStorageRes = localStorage.getItem(widgetKey);
-        const widgetConfig = JSON.parse(localStorageRes);
-
-        if(widgetConfig["innerCard"]["isSignedInStatus"] === "1") {
-            twitterSignInButtonEl.id = "Twitter-signOut";
-            twitterSignInButtonEl.innerText = "Sign Out";
-            $(window).unbind('storage');
-
+    var OauthInterval = setInterval(function() {
+        if(twitterOAuthWindow.closed) {
             let widgetKey = "Twitter";
-            let card = document.getElementById(widgetKey + "Id");
-            new twitterWidget("params").loadUserHomeTimeLine(card,widgetKey);
+            const localStorageRes = localStorage.getItem(widgetKey);
+            const widgetConfig = JSON.parse(localStorageRes);
+            if(widgetConfig["innerCard"]["isSignedInStatus"] === "1") {
+                twitterSignInButtonEl.id = "Twitter-signOut";
+                twitterSignInButtonEl.innerText = "Sign Out";
+
+                let card = document.getElementById(widgetKey + "Id");
+                new twitterWidget("params").loadUserHomeTimeLine(card,widgetKey);
+            }
+            clearInterval(OauthInterval);
         }
-    });
+    }, 1000);
 }
+
+// function checkSuccessfulOauth(e) {
+//     var twitterSignInButtonEl = document.getElementById(e.target.id);
+//
+//     /* Cross Origin Listener */
+//     // window.addEventListener('message', function(e) {
+//     //     var message = e.data;
+//     //     if(e.origin === "http://localhost:8000")
+//     //         console.log(message + " " + e.origin);
+//     // });
+//
+//     /* Local Storage Listner */
+//     $(window).unbind('storage');
+//     $(window).bind('storage', function (e) {
+//         widgetKey = "Twitter";
+//         const localStorageRes = localStorage.getItem(widgetKey);
+//         const widgetConfig = JSON.parse(localStorageRes);
+//
+//         if(widgetConfig["innerCard"]["isSignedInStatus"] === "1") {
+//             twitterSignInButtonEl.id = "Twitter-signOut";
+//             twitterSignInButtonEl.innerText = "Sign Out";
+//             $(window).unbind('storage');
+//
+//             let widgetKey = "Twitter";
+//             let card = document.getElementById(widgetKey + "Id");
+//             new twitterWidget("params").loadUserHomeTimeLine(card,widgetKey);
+//         }
+//     });
+// }
 
 function onSignOut() {
     widgetKey = "Twitter";
